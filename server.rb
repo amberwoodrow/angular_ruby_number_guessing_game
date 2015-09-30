e1de8a0bca71c4861e9f03d5fb0a6f3bdaa14dcc
amberwoodrow <amberwoodrow@gmail.com>
2015-09-29 22:09:28 -0600

    added some comments

--- server.rb
+++ server.rb
@@ -1,29 +1,29 @@
 require "json"
-require 'sinatra'
-enable :sessions
+require 'sinatra' # sinatra to run Ruby backend
+enable :sessions # sessions to store computer guess till user guesses the right number
 
 post '/guess' do
   guess = JSON.parse(request.body.read)["guess"].to_i
   result = check_user_guess guess
 
-  puts session[:server_guess]
-  {result: result}.to_json
+  puts session[:server_guess] # see what comp guess is for testng
+  {result: result}.to_json # create a json obj from hash to send result
 end
 
 get '/' do
-  session[:server_guess] = random_num # store comp guess for session
-  erb :index
+  session[:server_guess] = random_num # store computer/server guess for session
+  erb :index # render erb template on get "/"
 end
 
 
-def random_num
+def random_num 
   comp_num = Random.new
   comp_num.rand(100)
 end
 
-def check_user_guess user_guess
+def check_user_guess user_guess # validates the user's input and responds, valid or no
   if user_guess.between?(1, 100)
-    result = compare_guesses session[:server_guess], user_guess
+    result = compare_guesses session[:server_guess], user_guess 
   else
     result = "please enter a number 1-100"
   end
