ab572078054372c36af9ba7a7a70b4c33cb9cb5f
amberwoodrow <amberwoodrow@gmail.com>
2015-09-29 23:02:23 -0600

    fixed issues with turns variable

--- server.rb
+++ server.rb
@@ -6,7 +6,7 @@ post '/guess' do
   guess = JSON.parse(request.body.read)["guess"].to_i
   message = check_user_guess guess
 
-  session[:turns]+=1
+  session[:turns]
   puts session[:turns]
   puts session[:server_guess] # see what comp guess is for testng
   {result: message, turns: session[:turns]}.to_json # create a json obj from hash to send result
@@ -26,6 +26,7 @@ end
 
 def check_user_guess user_guess # validates the user's input and responds, valid or no
   if user_guess.between?(1, 20)
+    session[:turns]+=1
     compare_guesses session[:server_guess], user_guess 
   else
     "Please enter a number 1-20"
@@ -35,6 +36,7 @@ end
 def compare_guesses computer_guess, user_guess
   if user_guess == computer_guess
     session[:server_guess] = random_num
+    session[:turns] = 0
     "User wins! Enter a number to play again!"
   elsif user_guess.between?(computer_guess, computer_guess+3) || user_guess.between?(computer_guess-3, computer_guess)
     "You're close!"
